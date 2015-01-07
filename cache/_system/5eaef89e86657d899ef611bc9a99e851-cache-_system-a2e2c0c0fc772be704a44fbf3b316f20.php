16150394cf58f2534136b62896c77208bdba1313
Andre Borralho <andrefilipe_one@hotmail.com>
2015-01-08 00:09:03 +0100

    cache deleted?

--- cache/_system/5eaef89e86657d899ef611bc9a99e851-cache-_system-a2e2c0c0fc772be704a44fbf3b316f20.php
+++
@@ -1,1 +0,0 @@
-<?php die("Access Denied"); ?>#x#a:2:{s:6:"output";s:0:"";s:6:"result";O:8:"stdClass":4:{s:2:"id";s:5:"10215";s:6:"option";s:23:"com_isolation_solutions";s:6:"params";s:2:"{}";s:7:"enabled";s:1:"1";}}
