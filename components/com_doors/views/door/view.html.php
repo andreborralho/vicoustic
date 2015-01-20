d2befdee6d00cddda7553a737b60025369b5647c
Andre Borralho <andrefilipe_one@hotmail.com>
2015-01-20 20:47:51 +0100

    doors view files

--- components/com_doors/views/door/view.html.php
+++ components/com_doors/views/door/view.html.php
@@ -1,106 +1,76 @@
 <?php
 
-/**
- * @version     1.0.0
- * @package     com_doors
- * @copyright   Copyright (C) 2012. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- * @author      Andre <andrefilipe_one@hotmail.com> - http://
- */
-// No direct access
-defined('_JEXEC') or die;
-
-jimport('joomla.application.component.view');
-
-/**
- * View to edit
- */
-class DoorsViewDoor extends JView {
-
-    protected $state;
-    protected $item;
-    protected $form;
-    protected $params;
-
-    /**
-     * Display the view
-     */
-    public function display($tpl = null) {
-        
-		$app	= JFactory::getApplication();
-        $user		= JFactory::getUser();
-        
-        $this->state = $this->get('State');
-        $this->item = $this->get('Data');
-        $this->params = $app->getParams('com_doors');
-   		$this->items = $this->get('Items');
-
-        // Check for errors.
-        if (count($errors = $this->get('Errors'))) {
-            throw new Exception(implode("\n", $errors));
-        }
-        
-        
-        
-        if($this->_layout == 'edit') {
-            
-            $authorised = $user->authorise('core.create', 'com_doors');
-
-            if ($authorised !== true) {
-                throw new Exception(JText::_('JERROR_ALERTNOAUTHOR'));
-            }
-        }
-        
-        $this->_prepareDocument();
-
-        parent::display($tpl);
-    }
-
-
 	/**
-	 * Prepares the document
+	 * @version     1.0.0
+	 * @package     com_doors
+	 * @copyright   Copyright (C) 2012. All rights reserved.
+	 * @license     GNU General Public License version 2 or later; see LICENSE.txt
+	 * @author      Andre <andrefilipe_one@hotmail.com> - http://
 	 */
-	protected function _prepareDocument()
-	{
-		$app	= JFactory::getApplication();
-		$menus	= $app->getMenu();
-		$title	= null;
+// No direct access
+	defined('_JEXEC') or die;
 
-		// Because the application sets a default page title,
-		// we need to get it from the menu item itself
-		$menu = $menus->getActive();
-		if($menu)
-		{
-			$this->params->def('page_heading', $this->params->get('page_title', $menu->title));
-		} else {
-			$this->params->def('page_heading', JText::_('COM_DOORS_DEFAULT_PAGE_TITLE'));
-		}
-		$title = $this->params->get('page_title', '');
-		if (empty($title)) {
-			$title = $app->getCfg('sitename');
-		}
-		elseif ($app->getCfg('sitename_pagetitles', 0) == 1) {
-			$title = JText::sprintf('JPAGETITLE', $app->getCfg('sitename'), $title);
-		}
-		elseif ($app->getCfg('sitename_pagetitles', 0) == 2) {
-			$title = JText::sprintf('JPAGETITLE', $title, $app->getCfg('sitename'));
-		}
-		$this->document->setTitle($this->item->family . " - " . $this->item->name);
+	jimport('joomla.application.component.view');
+	require_once JPATH_COMPONENT . '/helpers/doors.php';
+	require_once JPATH_BASE . '/libraries/fpdf/fpdf.php';
 
-		if ($this->params->get('menu-meta_description'))
-		{
-			$this->document->setDescription($this->params->get('menu-meta_description'));
+	JFactory::getLanguage()->load('com_vicoustic', JPATH_SITE, 'en-GB');
+	$document = JFactory::getDocument();
+	$document->addScript('//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js');
+	$document->addScript('scripts/galleria-1.2.9.min.js');
+	$document->addScript('scripts/components.js');
+
+	class DoorsViewDoor extends JView {
+
+		protected $state;
+		protected $item;
+		protected $form;
+		protected $params;
+
+		public function display($tpl = null) {
+			$app	= JFactory::getApplication();
+
+			$this->state = $this->get('State');
+			$this->item = $this->get('Data');
+			$this->items = $this->get('Items');
+			$this->params = $app->getParams('com_doors');
+
+			// Check for errors.
+			if (count($errors = $this->get('Errors'))) {
+				throw new Exception(implode("\n", $errors));
+			}
+
+			$this->_prepareDocument();
+
+			parent::display($tpl);
 		}
 
-		if ($this->params->get('menu-meta_keywords'))
-		{
-			$this->document->setMetadata('keywords', $this->params->get('menu-meta_keywords'));
-		}
+		protected function _prepareDocument() {
+			$app	= JFactory::getApplication();
+			$menus	= $app->getMenu();
+			$title	= null;
 
-		if ($this->params->get('robots'))
-		{
-			$this->document->setMetadata('robots', $this->params->get('robots'));
+			// Because the application sets a default page title, we need to get it from the menu item itself
+			$menu = $menus->getActive();
+			if($menu) {
+				$this->params->def('page_heading', $this->params->get('page_title', $menu->title));
+			}
+			else {
+				$this->params->def('page_heading', JText::_('com_doors_DEFAULT_PAGE_TITLE'));
+			}
+
+			$this->document->setTitle($this->item->name);
+
+			if ($this->params->get('menu-meta_description')) {
+				$this->document->setDescription($this->params->get('menu-meta_description'));
+			}
+
+			if ($this->params->get('menu-meta_keywords')) {
+				$this->document->setMetadata('keywords', $this->params->get('menu-meta_keywords'));
+			}
+
+			if ($this->params->get('robots')) {
+				$this->document->setMetadata('robots', $this->params->get('robots'));
+			}
 		}
-	}        
-    
-}
+	}
